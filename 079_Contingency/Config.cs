704e4971eefb254b7ba662c1b62947a5fb4fdc84
Dyme <hugosimpson275@gmail.com>
2025-01-21 21:11:00 +0000

    init commit

---
+++ 079_Contingency/Config.cs
@@ -0,0 +1,8 @@
+﻿using Exiled.API.Interfaces;
+
+namespace _079_Contingency;
+
+public class Config : IConfig {
+    public bool IsEnabled { get; set; }
+    public bool Debug { get; set; }
+}
---
+++ 079_Contingency/PluginCore.cs
@@ -0,0 +1,29 @@
+﻿using Exiled.API.Features;
+
+namespace _079_Contingency;
+
+public class PluginCore : Plugin<Config> {
+    public PluginCore Instance;
+
+    public override void OnEnabled() {
+        Instance = new PluginCore();
+        RegisterEvents();
+        
+        base.OnEnabled();
+    }
+
+    public override void OnDisabled() {
+        UnregisterEvents();
+        Instance = null;
+        
+        base.OnDisabled();
+    }
+
+    void RegisterEvents() {
+        
+    }
+
+    void UnregisterEvents() {
+        
+    }
+}
